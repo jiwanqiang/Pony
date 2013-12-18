0abebbad6a2abb0931f400a35186be9600e71432
Wanqiang Ji <wanqiang.ji@gmail.com>
2013-12-18 23:38:57 +0800

    add __has_feature(objc_arc)

--- HHUIKit/HHPollView/HHPollView.m
+++ HHUIKit/HHPollView/HHPollView.m
@@ -25,7 +25,7 @@
 #import "HHPollView.h"
 
 #if ! __has_feature(objc_arc)
-	#warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
+	#error This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
 
 @interface HHPollView () <UIScrollViewDelegate>
