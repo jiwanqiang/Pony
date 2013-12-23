70fe39aea4d409161317ce93d24d9f49b7e15417
Wanqiang Ji <wanqiang.ji@gmail.com>
2013-12-23 11:54:00 +0800

    add example

---
+++ Projects/example/example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  example
+//
+//  Created by Wanqiang Ji on 12/23/13.
+//  Copyright (c) 2013 http://jiwanqiang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
