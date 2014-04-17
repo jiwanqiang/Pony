4345684508f747a2516b900a3d764d5c096d66ff
Wanqiang Ji <wanqiang.ji@gmail.com>
2014-04-17 13:54:54 +0800

    add EffectViewController

---
+++ Projects/example/example/Modules/EffectViewController.h
@@ -0,0 +1,13 @@
+//
+//  EffectViewController.h
+//  example
+//
+//  Created by Wanqiang Ji on 4/16/14.
+//  Copyright (c) 2014 http://jiwanqiang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EffectViewController : UITableViewController
+
+@end
