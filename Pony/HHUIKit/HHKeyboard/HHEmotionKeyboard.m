8f547d2cd704832967b8e7646460a2facb5a1ef9
Wanqiang Ji <wanqiang.ji@gmail.com>
2014-06-22 00:50:23 +0800

    fix bug

--- Pony/HHUIKit/HHKeyboard/HHEmotionKeyboard.m
+++ Pony/HHUIKit/HHKeyboard/HHEmotionKeyboard.m
@@ -32,7 +32,7 @@
                                      alpha:1.0]
 #endif
 
-#define HH_EXP_COUNT        85
+#define HH_EXP_COUNT        84
 #define HH_EXP_SIZE         44.f
 #define HH_EXP_HEAD         @"/s"
 #define HH_EXP_ROW_COUNT    4
@@ -72,17 +72,26 @@
         self.pageControl = [[UIPageControl alloc] initWithFrame:frame];
         [self addSubview:self.pageControl];
         
-        UIButton *delButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        NSString *path = [bundle stringByAppendingPathComponent:@"del_emoji_n.png"];
+        UIButton *sendButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        NSString *path = [bundle stringByAppendingPathComponent:@"VoiceInputBtn_ios7@2x.png"];
         UIImage *img = [UIImage imageWithContentsOfFile:path];
-        [delButton setImage:img forState:UIControlStateNormal];
-        path = [bundle stringByAppendingPathComponent:@"del_emoji_s.png"];
-        img = [UIImage imageWithContentsOfFile:path];
-        [delButton setImage:img forState:UIControlStateSelected];
-        SEL action = @selector(__pressDelbutton:);
-        [delButton addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
-        delButton.frame = CGRectMake(272.f, 182.f, 38.f, 28.f);
-        [self addSubview:delButton];
+        [sendButton setBackgroundImage:img forState:UIControlStateNormal];
+//        path = [bundle stringByAppendingPathComponent:@"del_emoji_s.png"];
+//        img = [UIImage imageWithContentsOfFile:path];
+//        [sendButton setImage:img forState:UIControlStateSelected];
+//        [sendButton setTitle:@"Send" forState:UIControlStateNormal];
+//        sendButton.titleLabel.font = [UIFont systemFontOfSize:14.f];
+        [sendButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        
+        NSDictionary *attr = @{NSFontAttributeName: [UIFont fontWithName:@"Courier-Oblique" size:14.f]};
+        NSAttributedString *str = [[NSAttributedString alloc] initWithString:@"发送"
+                                                                  attributes:attr];
+        [sendButton setAttributedTitle:str forState:UIControlStateNormal];
+//        SEL action = @selector(__pressDelbutton:);
+        SEL action = @selector(__pressSendButton:);
+        [sendButton addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
+        sendButton.frame = CGRectMake(252.f, 182.f, 70.f, 34.f);
+        [self addSubview:sendButton];
         
         [self __layoutExpressions];
 	}
@@ -116,7 +125,7 @@
 #pragma mark - Private Methods
 - (void)__layoutExpressions
 {
-    CGFloat width = (HH_EXP_COUNT / HH_EXP_PAGE_COUNT + 1) * 320.f;
+    CGFloat width = (HH_EXP_COUNT / HH_EXP_PAGE_COUNT) * 320.f;
     self.pageControl.numberOfPages = width / 320.f;
     self.expressionView.contentSize = CGSizeMake(width,
                                                  CGRectGetHeight(self.expressionView.bounds));
@@ -129,16 +138,24 @@
     for (int i = 1; i <= HH_EXP_COUNT; i++) {
         @autoreleasepool {
             btn = [UIButton buttonWithType:UIButtonTypeCustom];
-            btn.tag = i;
             btn.exclusiveTouch = YES;
             [btn addTarget:self action:action forControlEvents:UIControlEventTouchUpInside];
-            NSInteger pMod = ((i - 1) % HH_EXP_PAGE_COUNT);
-            NSInteger p = ((i - 1) / HH_EXP_PAGE_COUNT);
+            NSInteger pMod = i % HH_EXP_PAGE_COUNT;
+            NSInteger p = (i - 1) / HH_EXP_PAGE_COUNT;
+            
+            if (pMod == 0) {
+                btn.tag = 10001;
+                fileName = [NSString stringWithFormat:@"del_emoji_n.png"];
+            } else {
+                btn.tag = i;
+                fileName = [NSString stringWithFormat:@"%03d.png", i];
+            }
+            pMod = (i - 1) % HH_EXP_PAGE_COUNT;
+            
             CGFloat x = (pMod % HH_EXP_CLU_COUNT) * HH_EXP_SIZE + 6 + (p * 320);
             CGFloat y = (((i - 1) % HH_EXP_PAGE_COUNT) / HH_EXP_CLU_COUNT) * HH_EXP_SIZE + 8;
             btn.frame = CGRectMake( x, y, HH_EXP_SIZE, HH_EXP_SIZE);
             
-            fileName = [NSString stringWithFormat:@"%03d.png", i];
             path = [bundle stringByAppendingPathComponent:fileName];
             img = [UIImage imageWithContentsOfFile:path];
             [btn setImage:img forState:UIControlStateNormal];
@@ -150,23 +167,29 @@
 - (void)__pressDelbutton:(id)sender
 {
     NSString *text = self.textView.text;
-    if (text.length) {
-        NSString *finalText;
-        NSInteger textLength = text.length;
+    NSRange range = self.textView.selectedRange;
+    if (text.length && (range.location != 0) ) {
+        range.location -= 1;
+        range.length = 1;
+        NSMutableString *finalText = [NSMutableString stringWithString:text];
+        NSInteger textLength = range.location + 1;
         if (textLength >= HH_EXP_NAME_LENGTH) {
-            finalText = [text substringFromIndex:textLength - HH_EXP_NAME_LENGTH];
-            NSRange range = [finalText rangeOfString:HH_EXP_HEAD];
-            if (range.location == 0) {
-                NSRange range = [text rangeOfString:HH_EXP_HEAD options:NSBackwardsSearch];
-                finalText = [text substringToIndex:range.location];
+            NSString *tmpText = [text substringFromIndex:textLength - HH_EXP_NAME_LENGTH];
+            NSRange expRange = [tmpText rangeOfString:HH_EXP_HEAD];
+            if (expRange.location == 0) {
+                range.location -= (HH_EXP_NAME_LENGTH - 1);
+                range.length = HH_EXP_NAME_LENGTH;
+                [finalText deleteCharactersInRange:range];
             } else {
-                finalText = [text substringToIndex:textLength - 1];
+                [finalText deleteCharactersInRange:range];
             }
         } else {
-            finalText = [text substringToIndex:textLength - 1];
+            [finalText deleteCharactersInRange:range];
         }
         
         self.textView.text = finalText;
+        range.length = 0;
+        self.textView.selectedRange = range;
         if ([self.textView.delegate respondsToSelector:@selector(textViewDidChange:)]) {
             [self.textView.delegate textViewDidChange:self.textView];
         }
@@ -177,13 +200,38 @@
 {
     if (self.textView) {
         NSInteger i = ((UIButton *)sender).tag;
+        if (i == 10001) {
+            [self __pressDelbutton:sender];
+            return;
+        }
+        
         NSMutableString *text = [[NSMutableString alloc] initWithString:self.textView.text];
-        NSString *expressionText = [NSString stringWithFormat:@"%@%03zd", HH_EXP_HEAD, i];
-        [text appendString:expressionText];
+#if __LP64__ || (TARGET_OS_EMBEDDED && !TARGET_OS_IPHONE) || TARGET_OS_WIN32 || NS_BUILD_32_LIKE_64
+        NSString *expressionText = [NSString stringWithFormat:@"%@%03ld", HH_EXP_HEAD, i];
+#else
+        NSString *expressionText = [NSString stringWithFormat:@"%@%03d", HH_EXP_HEAD, i];
+#endif
+        NSUInteger loc = self.textView.selectedRange.location;
+        [text insertString:expressionText atIndex:loc];
         self.textView.text = text;
+        self.textView.selectedRange = NSMakeRange(loc + expressionText.length, 0);
         if ([self.textView.delegate respondsToSelector:@selector(textViewDidChange:)]) {
             [self.textView.delegate textViewDidChange:self.textView];
         }
+    }
+}
+
+- (void)__pressSendButton:(id)sender
+{
+    if (!self.textView) {
+        return;
+    }
+    
+    SEL sel = @selector(textView:shouldChangeTextInRange:replacementText:);
+    if ([self.textView.delegate respondsToSelector:sel]) {
+        [self.textView.delegate textView:self.textView
+                 shouldChangeTextInRange:NSMakeRange(0, 0)
+                         replacementText:@"\n"];
     }
 }
 
