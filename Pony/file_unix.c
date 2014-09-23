495098d29ff62d3d43a33649655b54d86334f217
Wanqiang Ji <wanqiang.ji@gmail.com>
2014-09-23 15:33:54 +0800

    modify

--- Pony/file_unix.c
+++ Pony/file_unix.c
@@ -1,8 +1,25 @@
 //
 //  file_unix.c
 //
-//  Created by Wanqiang Ji on 9/23/14.
-//  Copyright (c) 2014 Sina Inc. All rights reserved.
+//  Copyright (c) 2013 Ji Wanqiang
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+//  IN THE SOFTWARE.
 //
 //  Resolved the problem that LLVM compile the project in x86 environment.
 //    "_fwrite$UNIX2003", referenced from: Symbol(s) not found for architecture i386
